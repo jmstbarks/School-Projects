73a9c0587a5c9b403a65a27026fa3863fd735afb
Matthew England <matt.r.england@gmail.com>
2015-09-21 12:35:45 -0500

    Updated tester.c
    I added a more flexible test using the line macro and fixed a logic error when comparing floats.

--- test/tester.c
+++ test/tester.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <dyn_array.h>
 #include <string.h>
-
+#include <math.h>
 
 #include "../include/process_scheduling.h"
 
@@ -75,14 +75,14 @@ void test_load_process_control_blocks_from_file() {
 	dyn_array_t* tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	
 	// Test bad parameters
-	assert((load_process_control_blocks_from_file(NULL,NULL) == false),78);
-	assert((load_process_control_blocks_from_file(NULL,"\0") == false),79);
-	assert((load_process_control_blocks_from_file(tester,"Data/WrongPCBs.bin") == false),80);
-	assert((dyn_array_size(tester) == 0),81);
+	assert((load_process_control_blocks_from_file(NULL,NULL) == false),__LINE__);
+	assert((load_process_control_blocks_from_file(NULL,"\0") == false),__LINE__);
+	assert((load_process_control_blocks_from_file(tester,"Data/WrongPCBs.bin") == false),__LINE__);
+	assert((dyn_array_size(tester) == 0),__LINE__);
 
 	//Test good parameters
-	assert(load_process_control_blocks_from_file(tester,"../data/SamplePCBs.bin") == true,84);
-	assert((dyn_array_size(tester) == 3),85);
+	assert(load_process_control_blocks_from_file(tester,"../data/SamplePCBs.bin") == true,__LINE__);
+	assert((dyn_array_size(tester) == 3),__LINE__);
 	dyn_array_destroy(tester);
 
 }
@@ -95,18 +95,18 @@ void test_create_suspended_processes_and_assign_pcbs() {
 		[2] = {13,2,0,0}
 	};
 	dyn_array_t* tester = dyn_array_import(data,3,sizeof(ProcessControlBlock_t),NULL);
-	assert((create_suspended_processes_and_assign_pcbs(NULL) == false),98);
+	assert((create_suspended_processes_and_assign_pcbs(NULL) == false),__LINE__);
 	
 	for (size_t i = 0; i < 3; ++i) {
 		ProcessControlBlock_t* pcb = dyn_array_at(tester,i);
-		assert((!pcb->pid),102);
+		assert((!pcb->pid),__LINE__);
 
 	}
 
-	assert((create_suspended_processes_and_assign_pcbs(tester) == true),106);
+	assert((create_suspended_processes_and_assign_pcbs(tester) == true),__LINE__);
 	for (size_t i = 0; i < 3; ++i) {
 		ProcessControlBlock_t* pcb = dyn_array_at(tester,i);
-		assert((pcb->pid),109);
+		assert((pcb->pid),__LINE__);
 	}	
 	
 	dyn_array_destroy(tester);
@@ -128,17 +128,17 @@ void test_rearranged_process_control_blocks_by_arrival_time() {
 
 
 	dyn_array_t* tester = dyn_array_import(data,3,sizeof(ProcessControlBlock_t),NULL);
-	assert(rearranged_process_control_blocks_by_arrival_time(NULL) == false,131);
+	assert(rearranged_process_control_blocks_by_arrival_time(NULL) == false,__LINE__);
 	
 	for (size_t i = 0; i < 3; ++i) {
 		ProcessControlBlock_t* pcb = dyn_array_at(tester,i);
-		assert(pcb->arrivalTime == data[i].arrivalTime,135);
+		assert(pcb->arrivalTime == data[i].arrivalTime,__LINE__);
 	}
 
-	assert(rearranged_process_control_blocks_by_arrival_time(tester),138);
+	assert(rearranged_process_control_blocks_by_arrival_time(tester),__LINE__);
 	for (size_t i = 2; i > 0; --i) {
 		ProcessControlBlock_t* pcb = dyn_array_at(tester,i);
-		assert(pcb->arrivalTime == expected[i].arrivalTime,141);
+		assert(pcb->arrivalTime == expected[i].arrivalTime,__LINE__);
 	}	
 	
 	dyn_array_destroy(tester);
@@ -165,27 +165,27 @@ void test_fetch_new_processes() {
 	};
 	const size_t currentClockTime = 5;
 	// Bad input parameters
-	assert(fetch_new_processes(NULL,NULL,0) == false,168);
+	assert(fetch_new_processes(NULL,NULL,0) == false,1__LINE__);
 	// logic edge tests
 	dyn_array_t* newProcesses = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL); 
 	dyn_array_t* futureProcesses = dyn_array_import(data1,3,sizeof(ProcessControlBlock_t), NULL);  
-	assert(fetch_new_processes(newProcesses,futureProcesses,0) == true,172);
-	assert(dyn_array_size(newProcesses) == 3,173); 
-	assert(dyn_array_size(futureProcesses) == 0,174);
+	assert(fetch_new_processes(newProcesses,futureProcesses,0) == true,__LINE__);
+	assert(dyn_array_size(newProcesses) == 3,__LINE__); 
+	assert(dyn_array_size(futureProcesses) == 0,__LINE__);
 	dyn_array_clear(futureProcesses);
 	dyn_array_clear(newProcesses);
 
 	futureProcesses = dyn_array_import(data2,3,sizeof(ProcessControlBlock_t),NULL); 
-	assert(fetch_new_processes(newProcesses,futureProcesses,currentClockTime) == true,179);
-	assert(dyn_array_size(newProcesses) == 2,180);
-	assert(dyn_array_size(futureProcesses) == 1, 181);
+	assert(fetch_new_processes(newProcesses,futureProcesses,currentClockTime) == true,__LINE__);
+	assert(dyn_array_size(newProcesses) == 2,__LINE__);
+	assert(dyn_array_size(futureProcesses) == 1, __LINE__);
 	dyn_array_clear(futureProcesses);
 	dyn_array_clear(newProcesses);
 
 	futureProcesses = dyn_array_import(data3,3,sizeof(ProcessControlBlock_t),NULL); 
-	assert(fetch_new_processes(newProcesses,futureProcesses,currentClockTime) == false,186);
-	assert(dyn_array_size(newProcesses) == 0,187);
-	assert(dyn_array_size(futureProcesses) == 3,188);
+	assert(fetch_new_processes(newProcesses,futureProcesses,currentClockTime) == false,__LINE__);
+	assert(dyn_array_size(newProcesses) == 0,__LINE__);
+	assert(dyn_array_size(futureProcesses) == 3,__LINE__);
 	dyn_array_destroy(futureProcesses);
 	dyn_array_destroy(newProcesses);
 }
@@ -193,12 +193,12 @@ void test_fetch_new_processes() {
 
 void run_FCFS_test () {
 	ScheduleStats_t stats = first_come_first_served(NULL);
-	assert(stats.averageWallClockTime == 0,196);
-	assert(stats.averageLatencyTime == 0,197);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,1__LINE__);
 	dyn_array_t* tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	stats = first_come_first_served(tester);
-	assert(stats.averageWallClockTime == 0,200);
-	assert(stats.averageLatencyTime == 0,201);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 
 	ProcessControlBlock_t data[3] = {
 			[0] = {24,0,0,0},
@@ -211,8 +211,8 @@ void run_FCFS_test () {
 	create_suspended_processes_and_assign_pcbs(tester);
 	stats = first_come_first_served(tester);
 	float answers[2] = {27,17};
-	assert(stats.averageWallClockTime == answers[0],214);
-	assert(stats.averageLatencyTime == answers[1],215); 
+	assert(stats.averageWallClockTime == answers[0],__LINE__);
+	assert(stats.averageLatencyTime == answers[1],__LINE__); 
 	printf("AVG WALL CLOCK TIME = %f\nAVG LATENCY TIME = %f\n", stats.averageWallClockTime, stats.averageLatencyTime);
 	dyn_array_destroy(tester);
 }
@@ -221,12 +221,12 @@ void run_FCFS_test () {
 
 void run_SJF_test () {
 	ScheduleStats_t stats = shortest_job_first(NULL);
-	assert(stats.averageWallClockTime == 0,223);
-	assert(stats.averageLatencyTime == 0,224);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	dyn_array_t* tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	stats = shortest_job_first(tester);
-	assert(stats.averageWallClockTime == 0,228);
-	assert(stats.averageLatencyTime == 0,229);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	dyn_array_clear(tester);
 
 	ProcessControlBlock_t data[4] = {
@@ -242,8 +242,8 @@ void run_SJF_test () {
 	create_suspended_processes_and_assign_pcbs(tester);
 	stats = shortest_job_first(tester);
 	float answers[2] = {13,7};
-	assert(stats.averageWallClockTime == answers[0],245);
-	assert(stats.averageLatencyTime == answers[1],246);
+	assert(stats.averageWallClockTime == answers[0],__LINE__);
+	assert(stats.averageLatencyTime == answers[1],__LINE__);
 	printf("AVG WALL CLOCK TIME = %f\nAVG LATENCY TIME = %f\n", stats.averageWallClockTime, stats.averageLatencyTime);
 	dyn_array_destroy(tester);
 
@@ -251,12 +251,12 @@ void run_SJF_test () {
 
 void run_SRTF_test () {
 	ScheduleStats_t stats = shortest_remaining_time_first(NULL);
-	assert(stats.averageWallClockTime == 0,254);
-	assert(stats.averageLatencyTime == 0,255);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	dyn_array_t* tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	stats = shortest_remaining_time_first(tester);
-	assert(stats.averageWallClockTime == 0,258);
-	assert(stats.averageLatencyTime == 0,259);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	dyn_array_clear(tester);
 	
 	ProcessControlBlock_t data[4] = {
@@ -272,8 +272,8 @@ void run_SRTF_test () {
 	create_suspended_processes_and_assign_pcbs(tester);
 	stats = shortest_remaining_time_first(tester);
 	float answers[2] = {13,6.5};
-	assert(stats.averageWallClockTime == answers[0],275);
-	assert(stats.averageLatencyTime == answers[1],276);
+	assert(stats.averageWallClockTime == answers[0],__LINE__);
+	assert(stats.averageLatencyTime == answers[1],__LINE__);
 	printf("AVG WALL CLOCK TIME = %f\nAVG LATENCY TIME = %f\n", stats.averageWallClockTime, stats.averageLatencyTime);
 	dyn_array_destroy(tester);
 
@@ -281,12 +281,12 @@ void run_SRTF_test () {
 
 void run_RR_test () {		
 	ScheduleStats_t stats = shortest_remaining_time_first(NULL);
-	assert(stats.averageWallClockTime == 0,284);
-	assert(stats.averageLatencyTime == 0,285);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	dyn_array_t* tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	stats = shortest_remaining_time_first(tester);
-	assert(stats.averageWallClockTime == 0,288);
-	assert(stats.averageLatencyTime == 0,289);
+	assert(stats.averageWallClockTime == 0,__LINE__);
+	assert(stats.averageLatencyTime == 0,__LINE__);
 	tester = dyn_array_create(16,sizeof(ProcessControlBlock_t),NULL);
 	ProcessControlBlock_t data[3] = {
 			[0] = {24,0,0,0},
@@ -299,8 +299,8 @@ void run_RR_test () {
 	create_suspended_processes_and_assign_pcbs(tester);
 	stats = round_robin(tester, 4);
 	float answers[2] = {15.67,5.67};
-	assert(stats.averageWallClockTime == answers[0],302);
-	assert(stats.averageLatencyTime == answers[1],303);
+	assert(stats.averageWallClockTime == answers[0],__LINE__);
+	assert(fabs(stats.averageLatencyTime,answers[1]) < .00001,__LINE__);
 	printf("AVG WALL CLOCK TIME = %f\nAVG LATENCY TIME = %f\n", stats.averageWallClockTime, stats.averageLatencyTime);
 	dyn_array_destroy(tester);
 
