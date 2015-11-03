5c9f50021159a28b3a48a1f916ddbf54d90feac3
jtbc7d <jmstbarksdale27@gmail.com>
2015-11-03 04:28:07 -0600

    Update

---
+++ build/CMakeFiles/page_generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/page_generator.dir/src/page_generator.cpp.o"
+  "page_generator.pdb"
+  "page_generator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/page_generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/runner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runner.dir/test/runner.c.o"
+  "runner.pdb"
+  "runner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
